f19cc29c5a58de94cc59e73800c8c2f581fe1cf9
chien302 <chienvuong302@gmail.com>
2023-02-23 04:25:03 +0700

    interact

--- ssh-pexpect/pexpect1.py
+++ ssh-pexpect/pexpect1.py
@@ -1,35 +1,38 @@
 import pexpect
-
-PROMPT = ['# ', '>>> ', '> ', '\$ ']
+import sys
+PROMPT = ['# ','>>> ','> ','\$ ']
 
 def send_command(child, cmd):
-    child.sendline(cmd) 
-    child.expect(PROMPT)
-    print(child.before) 
+        child.sendline(cmd)
+        child.expect(PROMPT)
+        print (child.before)
 
 def connect(user, host, password):
-    ssh_newkey = 'Are you sure you want to connect to continue connecting'
-    connStr = 'ssh ' + user + '@' + host 
+    ssh_newkey = 'Are you sure you want to continue connecting'
+    connStr = 'ssh ' + user + '@' + host
     child = pexpect.spawn(connStr)
-    ret  = child.expect([pexpect.TIMEOUT, ssh_newkey, '[P|p]assword:'])
+    ret = child.expect([pexpect.TIMEOUT, ssh_newkey, 'password', pexpect.EOF])
+    if ret == 0:
+            print ('[-] Error Connecting')
+            return
     if ret == 1:
-        child.sendline('yes')
-        ret = child.expect('password:')
-    if ret != 0:
-        print ('[-] Error Connecting')
-        return 
+            child.sendline('yes')
+            ret = child.expect([pexpect.TIMEOUT, 'password', pexpect.EOF])
+            if ret == 0:
+                    print ('[-] ERROR Connecting')
+                    return
     child.sendline(password)
     child.expect(PROMPT)
     return child
+
 def main():
-    host = '192.169.0.212'
-    user = 'chien'
-    password = '123456'
-    child = connect(user, host, password)
-    if child is not None:
-        send_command(child)
-    else:
-        print ("Problem connecting!")
+        host = '192.168.0.171'
+        user = 'chien'
+        password = '123456'
+        child = connect(user, host , password)
+        # send_command(child, 'uname -a')
+        child.interact()
+
 
 if __name__ == '__main__':
-    main()
+        main()
