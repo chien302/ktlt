31af129b1fa5f539f83b0f374eddb929607822b8
chien <chien@chien.chien.com>
2023-02-23 00:07:52 +0700

    2-cm

--- ssh-pexpect/pexpect1.py
+++ ssh-pexpect/pexpect1.py
@@ -6,29 +6,30 @@ def send_command(child, cmd):
     child.sendline(cmd) 
     child.expect(PROMPT)
     print(child.before) 
+
 def connect(user, host, password):
     ssh_newkey = 'Are you sure you want to connect to continue connecting'
-    connStr = 'ssh' + user + '@' + host 
+    connStr = 'ssh ' + user + '@' + host 
     child = pexpect.spawn(connStr)
     ret  = child.expect([pexpect.TIMEOUT, ssh_newkey, '[P|p]assword:'])
-    if ret == 0:
-        print('[-] Error connecting')
-        return
     if ret == 1:
         child.sendline('yes')
-        ret = child.expect([pexpect.TIMEOUT, '[P|p]assword:'])
-        if ret == 0:
-            print ('[-] Error Connecting')
-            return
+        ret = child.expect('password:')
+    if ret != 0:
+        print ('[-] Error Connecting')
+        return 
     child.sendline(password)
     child.expect(PROMPT)
     return child
 def main():
-    host = 'localhost'
-    user = 'root'
-    password = 'toor'
+    host = '192.169.0.212'
+    user = 'chien'
+    password = '123456'
     child = connect(user, host, password)
-    send_command(child, 'cat /etc/shadow | grep root')
+    if child is not None:
+        send_command(child)
+    else:
+        print ("Problem connecting!")
 
 if __name__ == '__main__':
     main()
